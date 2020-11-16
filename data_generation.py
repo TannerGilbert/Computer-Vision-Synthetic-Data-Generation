22d8a5cd136af0c153aca2255797e23155f833d9
TannerGilbert <gilberttanner.work@gmail.com>
2020-11-16 19:29:11 +0100

    implemented collision avoidance (based on https://github.com/basedrhys/cocosynth/commit/d009a0de17b154ca3b469e8d4c0a7afa8fa51271)

--- data_generation.py
+++ data_generation.py
@@ -15,7 +15,7 @@ import albumentations as A
 
 class SyntheticImageGenerator:
 
-    def __init__(self, input_dir: str, output_dir: str, image_number: int, max_objects_per_image: int, image_width: int, image_height: int, augmentation_path: str, scale_foreground_by_background_size: bool):
+    def __init__(self, input_dir: str, output_dir: str, image_number: int, max_objects_per_image: int, image_width: int, image_height: int, augmentation_path: str, scale_foreground_by_background_size: bool, avoid_collisions: bool):
         self.input_dir = Path(input_dir)
         self.output_dir = Path(output_dir)
         self.image_number = image_number
@@ -25,6 +25,7 @@ class SyntheticImageGenerator:
         self.zero_padding = 8
         self.augmentation_path = Path(augmentation_path)
         self.scale_foreground_by_background_size = scale_foreground_by_background_size
+        self.avoid_collisions = avoid_collisions
 
         self._validate_input_directory()
         self._validate_output_directory()
@@ -138,6 +139,8 @@ class SyntheticImageGenerator:
         annotations['imageWidth'] = self.image_width
         annotations['imageHeight'] = self.image_height
 
+        fg_list = []
+
         for fg in foregrounds:
             fg_image = Image.open(fg['image_path'])
 
@@ -176,6 +179,41 @@ class SyntheticImageGenerator:
             assert max_x_position >= 0 and max_y_position >= 0, \
                 f'foreground {fg["image_path"]} is too big ({fg_image.size[0]}x{fg_image.size[1]}) for the requested output size ({self.image_width}x{self.image_height}), check your input parameters'
             foreground_position = (random.randint(0, max_x_position), random.randint(0, max_y_position))
+
+            # avoid collisions of foreground objects (based on https://github.com/basedrhys/cocosynth/commit/d009a0de17b154ca3b469e8d4c0a7afa8fa51271)
+            if self.avoid_collisions:
+                fg_rect = [foreground_position[0],  # x1
+                           foreground_position[1],  # y1
+                           foreground_position[0] + fg_image.size[0],  # x2
+                           foreground_position[1] + fg_image.size[1]]  # y2
+
+                visited_centroids = []
+                colliding_point = self._is_colliding(fg_rect, fg_list)
+
+                while colliding_point is not None:
+                    # Move the fg away from the colliding point
+                    step_size = 50
+                    curr_centroid_x = int((fg_rect[0] + fg_rect[2]) / 2)
+                    curr_centroid_y = int((fg_rect[1] + fg_rect[3]) / 2)
+                    new_centroid_pos = self._get_new_centroid_pos(colliding_point,
+                                                                  (curr_centroid_x, curr_centroid_y),
+                                                                  step_size)
+
+                    if self._visited_point_before(new_centroid_pos, visited_centroids):
+                        print("Tried to re-visit point {}".format(new_centroid_pos))
+                        fg_rect = None
+                        break
+                    visited_centroids.append(new_centroid_pos)
+
+                    fg_rect = self._get_rect_position(new_centroid_pos, fg_image)
+                    colliding_point = self._is_colliding(fg_rect, fg_list)
+
+                if fg_rect is None or self._outside_img(composite, fg_rect):
+                    # print("Outside image {}".format(fg_rect))
+                    continue
+                else:
+                    paste_position = (int(fg_rect[0]), int(fg_rect[1]))
+                    fg_list.append(fg_rect)
 
             # Create a new foreground image as large as the composite and paste it on top
             new_fg_image = Image.new('RGBA', composite.size, color=(0, 0, 0, 0))
@@ -222,6 +260,93 @@ class SyntheticImageGenerator:
 
         return composite, annotations
 
+    def _get_point_to_move_from(self, colliding_centroids):
+        '''
+        Average all the centroid locations that the fg was colliding with
+        This gives a point for the fg to move away from
+        input: array of centroids -> [(200, 100), (50, 20), ...]
+        '''
+        return np.mean(colliding_centroids, 0)
+
+    def _is_colliding(self, fg_rect, fg_rect_list):
+        '''
+        Check if the current foreground object is colliding with any foregrounds
+        we've placed on the image already. The overlap threshold controls how much overlap
+        of the rectangles is allowed. The overlap is a proportion of the total area of this
+        foreground object, as opposed to an absolute value
+        '''
+        overlap_thresh = 0.3
+
+        colliding_centroids = []
+
+        for rect in fg_rect_list:
+            x1, y1, x2, y2 = fg_rect
+            comp_x1, comp_y1, comp_x2, comp_y2 = rect
+            x_overlap = max(0, min(comp_x2, x2) - max(comp_x1, x1))
+            y_overlap = max(0, min(comp_y2, y2) - max(comp_y1, y1))
+
+            rect_area = (x2 - x1) * (y2 - y1)
+            total_overlap_area = x_overlap * y_overlap
+
+            prop_overlap = total_overlap_area / float(rect_area)
+
+            if prop_overlap > overlap_thresh:
+                colliding_centroid = (comp_x2 - comp_x1, comp_y2 - comp_y1)
+                colliding_centroids.append(colliding_centroid)
+
+        if len(colliding_centroids) == 0:
+            return None
+        else:
+            return self._get_point_to_move_from(colliding_centroids)
+
+    def _get_new_centroid_pos(self, pt_a, pt_b, step_size):
+        '''
+        https://math.stackexchange.com/questions/175896/finding-a-point-along-a-line-a-certain-distance-away-from-another-point
+        Given the point to move from (pt_a), and our current centroid (pt_b),
+        move further away along the line i.e.:
+        a -------- b --<step_size>-- new_point
+        '''
+        v = np.subtract(pt_b, pt_a)
+        norm_v = v / np.linalg.norm(v)
+
+        new_point = tuple(map(int, pt_b + (step_size * norm_v)))  # Convert to an int tuple
+        # print("Moving from {} to {}".format(pt_b, new_point))
+        return new_point
+
+    def _get_rect_position(self, centroid, fg_image):
+        '''
+        Convert a centroid to the corresponding rectangle, given a foreground image
+        '''
+        width, height = fg_image.size
+        x1 = centroid[0] - (width / 2)
+        y1 = centroid[1] - (height / 2)
+        x2 = centroid[0] + (width / 2)
+        y2 = centroid[1] + (height / 2)
+        return [x1, y1, x2, y2]
+
+    def _visited_point_before(self, coll_pt, coll_pts):
+        '''
+        Check if we've visited this point before.
+        Stops collision avoidance from getting stuck in between a group of points
+        '''
+        for pt in coll_pts:
+            if coll_pt[0] == pt[0] and coll_pt[1] == pt[1]:
+                return True
+
+        return False
+
+    def _outside_img(self, img, fg_rect):
+        '''
+        Don't paste the foreground if the centroid is outside the image
+        '''
+        curr_centroid_x = int((fg_rect[0] + fg_rect[2]) / 2)
+        curr_centroid_y = int((fg_rect[1] + fg_rect[3]) / 2)
+
+        return (curr_centroid_x < 0 or
+                curr_centroid_x > img.size[0] or
+                curr_centroid_y < 0 or
+                curr_centroid_y > img.size[1])
+
     def _transform_foreground(self, fg_image):
         if self.transforms:
             return Image.fromarray(self.transforms(image=np.array(fg_image))['image'])
@@ -243,9 +368,10 @@ if __name__ == '__main__':
     parser.add_argument('--max_objects_per_image', type=int, default=3, help='Maximum number of objects per images')
     parser.add_argument('--image_width', type=int, default=640, help='Width of the output images')
     parser.add_argument('--image_height', type=int, default=480, help='Height of the output images')
-    parser.add_argument('--scale_foreground_by_background_size', default=True, action='store_false', help='Whether the foreground images should be scaled based on the background size')
+    parser.add_argument('--scale_foreground_by_background_size', default=True, action='store_false', help='Whether the foreground images should be scaled based on the background size (default=true)')
+    parser.add_argument('--avoid_collisions', default=True, action='store_false', help='Whether or not to avoid collisions (default=true)')
 
     args = parser.parse_args()
 
-    data_generator = SyntheticImageGenerator(args.input_dir, args.output_dir, args.image_number, args.max_objects_per_image, args.image_width, args.image_height, args.augmentation_path, args.scale_foreground_by_background_size)
+    data_generator = SyntheticImageGenerator(args.input_dir, args.output_dir, args.image_number, args.max_objects_per_image, args.image_width, args.image_height, args.augmentation_path, args.scale_foreground_by_background_size, args.avoid_collisions)
     data_generator.generate_images()
